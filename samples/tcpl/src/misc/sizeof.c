11b450e871e80e6eb96072fc3a83a677515b3e95
Wenliang ZHANG <zedware@gmail.com>
2021-09-28 18:53:32 +0800

    Update sizeof.c

--- samples/tcpl/src/misc/sizeof.c
+++ samples/tcpl/src/misc/sizeof.c
@@ -4,6 +4,9 @@
 //   $ gcc -Wall sizeof.c
 #include <stdio.h>
 
+// 1. #x can concat strings
+// 2. %lu for unsigned long int (aka. size_t)
+// 3. \ to join the lines
 #define SZ(x)  { \
     printf("sizeof(" #x ") = %lu\n", sizeof(x)); \
   }
@@ -17,7 +20,35 @@ void foo(void) {
     SZ(double);
 }
 
+// Count the number of elements in an array x.
+#define countof(x)  (sizeof(x)/sizeof(x[0]))
+
+// Not work as expected.
+void bar(void) {
+    const char *types[] = {
+       "char",
+       "short",
+       "int",
+       "long",
+       "float",
+       "double"
+    };
+
+    // 1. sizeof(types) is the bytes of the array types[].
+    // 2. sizeof(types[0]) is the bytes of an element of array: types[0]
+    for (size_t i = 0; i < countof(types); i++) {
+        SZ(types[i]);
+    }
+
+    // Not worked either.
+    for (size_t i = 0; i < countof(types); i++) {
+        printf("sizeof(%s) = %lu\n", types[i], sizeof(types[i]));
+    }
+}
+
 int main(void) {
     foo();
+    bar();
     return 0;
 }
+
