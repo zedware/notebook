6c9f322d0cf858d077636ed64a3b2e2014ce2b19
Wenliang Zhang <zedware@gmail.com>
2023-12-07 00:20:26 +0800

    Update minigrep

--- samples/rust/minigrep/src/main.rs
+++ samples/rust/minigrep/src/main.rs
@@ -5,24 +5,35 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     dbg!(&args);
-    let (query, file) = parse_args(&args);
-    grep_file(query, file);
+    let config = Config::new(&args);
+    grep_file(config);
 }
 
-fn parse_args(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    file: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        parse_args(args)
+    }
+}
+
+fn parse_args(args: &[String]) -> Config {
     if args.len() != 2 {
         eprintln!("Arguments not equal to 2!");
         process::exit(1);
     }
 
-    let query = &args[0];
-    let file = &args[1];
-    (query, file)
+    let query = args[0].clone();
+    let file = args[1].clone();
+    Config {query, file}
 }
 
-fn grep_file(query: &str, file: &str) -> () {
-    println!("Search for: {query}\n");
-    let contents = fs::read_to_string(file)
+fn grep_file(config: Config) -> () {
+    println!("Search for: {}\n", config.query);
+    let contents = fs::read_to_string(config.file)
         .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
 }
