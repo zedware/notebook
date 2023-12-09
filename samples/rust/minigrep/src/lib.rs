43b672ef5b7f538a86064287a17fe64613b99653
Wenliang Zhang <zedware@gmail.com>
2023-12-10 00:58:07 +0800

    minigrep: add unittests

--- samples/rust/minigrep/src/lib.rs
+++ samples/rust/minigrep/src/lib.rs
@@ -25,8 +25,58 @@ fn parse_args(args: &[String]) -> Result<Config, &'static str> {
 }
 
 pub fn grep_file(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Search for: {}\n", config.query);
-    let contents = fs::read_to_string(config.file)?;
+    println!("Search for: \n{}\n", config.query);
+    let contents = fs::read_to_string(config.file).unwrap();
     println!("With text:\n{contents}");
+    println!("Found:");
+    let mut i = 0;
+    for line in grep_string(&config.query, &contents) {
+        println!("{line}");
+        i = i + 1;
+    }
+    println!("\n{} lines found.", i);
     Ok(())
+}
+
+fn grep_string<'a>(query: &str, s: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in s.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "swift";
+        let contents = "\
+taylor swift is \
+a famous singer.";
+        assert_eq!(vec!["taylor swift is a famous singer."], grep_string(query, contents));
+    }
+
+    #[test]
+    fn two_results() {
+        let query = "rust";
+        let contents = "\
+rust for dust.\n\
+rust for steel.\n\
+bye.";
+        assert_eq!(vec!["rust for dust.", "rust for steel."], grep_string(query, contents));
+    }
+
+    #[test]
+    fn rust_the_best() {
+        let query = "rust";
+        let contents = "rust is better than everyone else.";
+        assert_eq!(vec!["rust is better than everyone else."], grep_string(query, contents));
+    }
 }
