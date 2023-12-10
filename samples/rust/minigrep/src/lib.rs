c250bf4f7976c66ee4520aaf263e52b1e43c0526
Wenliang Zhang <zedware@gmail.com>
2023-12-10 23:38:21 +0800

    minigrep: ignore case by env variable

--- samples/rust/minigrep/src/lib.rs
+++ samples/rust/minigrep/src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     query: String,
     file: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -21,18 +23,29 @@ fn parse_args(args: &[String]) -> Result<Config, &'static str> {
 
     let query = args[0].clone();
     let file = args[1].clone();
-    Ok(Config { query, file })
+    let ignore_case: i32 = env::var("IGNORE_CASE").unwrap_or_default().parse().unwrap_or_default();
+    let ignore_case = if ignore_case != 0 { true } else { false };
+    println!("Environment variable: {}, bool: {}", env::var("IGNORE_CASE").unwrap_or_default(), ignore_case);
+    Ok(Config { query, file, ignore_case })
 }
 
 pub fn grep_file(config: Config) -> Result<(), Box<dyn Error>> {
     println!("Search for: \n{}\n", config.query);
-    let contents = fs::read_to_string(config.file).unwrap();
+    let contents = fs::read_to_string(config.file)?;
     println!("With text:\n{contents}");
-    println!("Found:");
     let mut i = 0;
-    for line in grep_string(&config.query, &contents) {
-        println!("{line}");
-        i = i + 1;
+    if config.ignore_case {
+        println!("Found igore case:");
+        for line in grep_string_lowercase(&config.query, &contents) {
+            println!("{line}");
+            i = i + 1;
+        }
+    } else {
+        println!("Found case sensitive:");
+        for line in grep_string(&config.query, &contents) {
+            println!("{line}");
+            i = i + 1;
+        }
     }
     println!("\n{} lines found.", i);
     Ok(())
@@ -43,6 +56,19 @@ fn grep_string<'a>(query: &str, s: &'a str) -> Vec<&'a str> {
 
     for line in s.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+fn grep_string_lowercase<'a>(query: &str, s: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in s.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
--- samples/rust/minigrep/src/main.rs
+++ samples/rust/minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     dbg!(&args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::grep_file(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
